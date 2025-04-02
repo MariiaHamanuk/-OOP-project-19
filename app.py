41c8027fcadd66a5c2ab6dac903ecf63643803fa
Nestor Leyko <leiko.pn@ucu.edu.ua>
2025-04-03 01:17:36 +0200

    reworked for postgresql

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
 '''back-end'''
 import json
+import re
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+# from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
 app = Flask(__name__)
 app.secret_key = "very_secure123"
 
-DATA = "users_data.json"
-EVENTS = "events.json"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:very_secure321@localhost:5432/base'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    '''Users table'''
+    id = db.Column(db.Integer, primary_key=True)
+    occupation = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(320), unique=True, nullable=False)
+    number = db.Column(db.String(320), unique=True)
+    name = db.Column(db.String(50), nullable=True)
+    surname = db.Column(db.String(50), nullable=True)
+    bio = db.Column(db.String(500))
+    password = db.Column(db.Text, nullable=False)
+    verified = db.Column(db.Boolean)
+
+    def __init__(self, occupation, username, email, number, name, surname, bio, password, verified):
+        '''for positional arguments'''
+        self.occupation = occupation
+        self.username = username
+        self.email = email
+        self.number = number
+        self.name = name
+        self.surname = surname
+        self.bio = bio
+        self.password = password #add hashing
+        self.verified = verified
+
+class Event(db.Model):
+    '''Event table'''
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    picture = db.Column(db.Text)
+    accepted = db.Column(db.Boolean)
+
+    def __init__(self, title, description, picture):
+        '''for positional arguments'''
+        self.title = title
+        self.description = description
+        self.picture = picture
+        self.accepted = False
 
 
 
@@ -41,15 +87,15 @@ def logout_page():
 @app.route("/main")
 def main():
     '''main page'''
-    data = read_file(DATA)
-    occupation = data[0][session['user']]["occupation"]
+    user = User.query.filter_by(username=session['user']).first()
+    occupation = user.occupation
     return render_template("main.html", occupation=occupation)
 
 @app.route("/calendar")
 def calendar():
     '''calendar page'''
-    data = read_file(DATA)
-    occupation = data[0][session['user']]["occupation"]
+    user = User.query.filter_by(username=session['user']).first()
+    occupation = user.occupation
     return render_template("calendar.html", occupation=occupation)
 
 @app.route("/questionnaire")
@@ -78,9 +124,9 @@ def ps_register():
     '''sign up page'''
     return render_template("ps-register.html")
 
-@app.route('/add-event')
+@app.route('/sign-in-volunteer')
 def v_register():
-    '''add event page'''
+    '''sign up page'''
     return render_template('v-register.html')
 
 @app.route("/login")
@@ -98,84 +144,84 @@ def v_approving():
     '''after sending event page'''
     return render_template('v-approving.html')
 
-#sing up/in
+
 
 @app.route("/validation", methods=["POST"])
 def validate_user():
     '''validates user'''
     username = request.form["username"]
     password = request.form["password"]
-    data = read_file(DATA)
-    if not username in data[0]:
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
         return render_template('login.html', error_message="No account with this username")
 
-    if data[0][username]['password'] == password:
-        if not "accepted" in data[0][username]:
+    if user.password == password:
+        if user.verified:
             session["user"] = username
             return redirect(url_for("main"))
+
+        return render_template("login.html", error_message="Wait for the verification")
+
     return render_template("login.html", error_message="Wrong password")
 
 @app.route("/save", methods=["POST"])
-def save_data():
+def save_user():
     '''saves user to file'''
     occupation = request.form["occupation"]
 
-    username = request.form.get("username")
-    params = ["occupation", "password"]
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
 
-    if occupation == "psychologist":
-        params.extend(["name", "surname", "bio", "email", "number", "accepted"])
+    name = request.form.get("name")
+    surname = request.form.get("surname")
+    bio = request.form.get("bio")
+    number = re.sub(r"[^0-9]", "", request.form.get("number")) \
+if request.form.get("number") else None
 
-    elif occupation == "volunteer":
-        event = request.form["event-name"]
-        info = {"description": request.form['description'], \
-"email": request.form['email'], "number": request.form['number']}
-        write_to_json(event, info, EVENTS)
-        return redirect(url_for("v_approving"))
+    if occupation == 'military':
+        user = User(occupation, username, email, number, name, surname, bio, password, True)
 
-    info = {param : request.form.get(param, False) for param in params}
+    else:
+        user = User(occupation, username, email, number, name, surname, bio, password, False)
+
     if not used(username):
-        write_to_json(username, info, DATA)
+        add_to_db(user)
+        session["user"] = username
+        return redirect(url_for("main"))
 
-        if occupation == "military":
-            session["user"] = username
-            return redirect(url_for("main"))
-        return redirect(url_for("ps_approving"))
+    match occupation:
+        case 'military':
+            page = "m-register.html"
+        case "psychologist":
+            page = "ps-register.htmlr"
+        case "volunteer":
+            page = "v-register.html"
 
-    if occupation == "military":
-        return render_template("m-register.html", \
-error_message="Account with this username already exists")
-    return render_template("ps-register.html", \
-error_message="Account with this username already exists")
+    return render_template(page, error_message="Account with this username already exists")
 
 def used(username):
     '''checks if name is already in use'''
-    data = read_file(DATA)
-    return username in data[0]
+    return User.query.filter_by(username=username).first()
 
-def read_file(filename):
-    '''loads json file'''
-    with open(filename, encoding="utf-8") as f:
-        try:
-            data = json.load(f)
-        except Exception:
-            data = [{}]
-    return data
-
-def write_to_json(key, info, filename):
-    '''writes data to file'''
-    data = read_file(filename)
-    data[0][key] = info
-
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2)
+def add_to_db(data):
+    '''writes data to the database'''
+    db.session.add(data)
+    db.session.commit()
 
 @app.route("/complete-logout")
 def logout():
     '''logout function'''
-    session.pop("user",None)
+    session.pop("user", None)
     session.clear()
     return redirect(url_for("start"))
 
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
 if __name__ == "__main__":
+    create_tables()
     app.run(debug=True)
